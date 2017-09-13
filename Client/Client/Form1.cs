fe3f7d38d00f4446301a916143299f5ca909718e
usm4nq <usm4nq@gmail.com>
2017-09-13 04:07:51 -0700

    Commited the source files

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        
+        string serverIp = "localhost";
+        int port = 8080;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient(serverIp, port);
+
+            int byteCount = Encoding.ASCII.GetByteCount(textBox1.Text);
+            
+            byte[] sendBuffer = new byte[byteCount];
+            byte[] recieveData = new byte[100];
+
+            sendBuffer = Encoding.ASCII.GetBytes(textBox1.Text);
+            NetworkStream stream = client.GetStream();
+            stream.Write(sendBuffer,0,sendBuffer.Length);
+            stream.Read(recieveData, 0, recieveData.Length);
+            StringBuilder msg = new StringBuilder();
+
+            foreach (byte b in recieveData)
+            {
+                if (b.Equals(00))
+                {
+                    break;
+                }
+                else
+                    msg.Append(Convert.ToChar(b).ToString());
+            }
+
+            label1.Text = msg.ToString() + msg.Length;
+            stream.Close();
+        }
+    }
+}
---
+++ Server/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        /********************************************************************
+         * 
+         *  each client should have its own object in the server so that we know to which client the data should be sent
+         *  
+         *  and is there any way to create a tcp client in Javascript?
+         *
+         *  please help
+         */
+
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8080);
+            TcpClient client = default(TcpClient);
+
+            string sms = "recieved";
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server Started");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Error!");
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+            }
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                byte[] recievedData = new byte[100];
+
+
+                int byteCount = Encoding.ASCII.GetByteCount(sms);
+                byte[] sendData = new byte[byteCount];
+                sendData = Encoding.ASCII.GetBytes(sms);
+
+
+                NetworkStream stream = client.GetStream();
+                
+                
+
+                stream.Read(recievedData, 0, recievedData.Length);
+                stream.Write(sendData,0,sendData.Length);
+
+                StringBuilder msg = new StringBuilder();
+
+                foreach (byte b in recievedData)
+                {
+                    if (b.Equals(00))
+                    {
+                        break;
+                    }
+                    else
+                        msg.Append(Convert.ToChar(b).ToString());
+                }
+
+                Console.WriteLine(msg.ToString() + msg.Length);
+            }
+        }
+    }
+}
